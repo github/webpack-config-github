091032bbacb9f19157d43c1af98f79fb474bbf5e
Joshua Peek <josh@joshpeek.com>
2017-11-15 18:01:09 -0800

    Configure output bundle names

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,17 @@ const path = require('path')
 module.exports = () => {
   const cwd = process.cwd()
 
-  const entry = ['./index.js', './src/index.js']
+  const indexEntry = ['./index.js', './src/index.js']
     .map(entry => path.resolve(cwd, entry))
     .find(filename => fs.existsSync(filename))
 
   return {
-    entry,
+    entry: {
+      index: indexEntry
+    },
     output: {
-      filename: 'dist/bundle.js'
+      filename: '[name].bundle.js',
+      path: path.resolve(cwd, 'dist')
     }
   }
 }
