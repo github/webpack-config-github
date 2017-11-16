f9e58198ca640b77b80978df0adfe29e776e9ff6
Joshua Peek <josh@joshpeek.com>
2017-11-15 17:57:47 -0800

    Clean up implicit functions

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,8 @@ module.exports = () => {
   const cwd = process.cwd()
 
   const entry = ['./index.js', './src/index.js']
-    .map(entry => {
-      return path.resolve(cwd, entry)
-    })
-    .find(filename => {
-      return fs.existsSync(filename)
-    })
+    .map(entry => path.resolve(cwd, entry))
+    .find(filename => fs.existsSync(filename))
 
   return {
     entry,
