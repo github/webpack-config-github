e73aa31eec2506efc84f30f9714fdce79289259f
Caged <justin@labratrevenge.com>
2018-07-31 11:39:12 -0700

    fix syntax for config in csp example

--- examples/custom-csp/webpack.config.js
+++ examples/custom-csp/webpack.config.js
@@ -2,10 +2,11 @@
 
 const config = require('../..') // require('webpack-config-github')
 
-module.exports = (env /*: string */) =>
-  config(env, {
+module.exports = (env /*: string */) => {
+  return config(env, {
     cspDirectives: {
       // Allow local fonts and Google fonts
       fontSrc: ["'self' https://fonts.gstatic.com"]
     }
   })
+}
